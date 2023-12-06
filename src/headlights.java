4170ed1cd316f3e5f0a88338e62942b4cd69bb6c
saredk <sare.sogut@gmail.com>
2023-12-06 02:13:35 +0000

    third commit

--- src/headlights.java
+++ src/headlights.java
@@ -1,5 +1,8 @@
 public class headlights {
     public static void main(String[] args) {
         System.out.println("Hello Batch18");
+        System.out.println("Hello Batch18");System.out.println("Hello Batch18");System.out.println("Hello Batch18");
+
+
     }
 }
